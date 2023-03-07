770aa6cde802ffc53d07b9df1f7516d8227773ab
reito <cnschwarzer@qq.com>
2023-03-07 14:24:17 +0800

    Update inference_main.py

--- inference_main.py
+++ inference_main.py
@@ -22,7 +22,7 @@ svc_model = Svc(model_path, config_path)
 infer_tool.mkdir(["raw", "results"])
 
 # 支持多个wav文件，放在raw文件夹下
-clean_names = ["66002"]
+clean_names = [sys.argv[2]]
 trans = [0]  # 音高调整，支持正负（半音）
 spk_list = ['grm']  # 每次同时合成多语者音色
 slice_db = -40  # 默认-40，嘈杂的音频可以-30，干声保留呼吸可以-50
